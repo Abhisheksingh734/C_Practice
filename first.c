f77486358ebea917067d340c47ec4bb340c35981
Abhishek <abhisheksinghasr734@gmail.com>
2023-02-02 01:06:20 +0530

    first c

---
+++ first.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <conio.h>
+
+
+// // global declaration of function 
+// // void display();
+
+// // main function
+// void main()
+// {
+//     int a = 10;
+//     a=50;
+//     printf("%d",a);
+//     printf("Hii guys");
+//     // calling of function
+//     // display();
+
+//     //to hold the output
+//     // getch();
+// }
+
+
+// void display()
+// {
+//     printf("Abhishek here");
+//     printf("%c",65);
+// }
+
+void main(){
+    char int_type =98;
+    printf("%c",int_type);
+}
